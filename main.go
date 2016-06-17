f7c4c62f994859531b362d51ee9bd3f35fd6e09d
Simas Cepaitis <simas.cepaitis@pearson.com>
2016-06-17 15:03:01 +0300

    Add error check

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 // import "log"
-import "net/http"
-import "os"
-import "github.com/gorilla/handlers"
-
+import (
+ "net/http"
+ "os" 
+ "github.com/gorilla/handlers"
+ "log"
+)
 
 func main() {
-    listenAddress := ":8080"
-    optPort := os.Getenv("LISTEN_PORT")
-    if optPort != "" {
-      listenAddress  = ":" + os.Getenv("LISTEN_PORT")
-    }
+  listenAddress := ":8080"
+  optPort := os.Getenv("LISTEN_PORT")
+  if optPort != "" {
+    listenAddress  = ":" + os.Getenv("LISTEN_PORT")
+  }
 
-    logged := handlers.CombinedLoggingHandler(os.Stderr, Handlers() )
+  logged := handlers.CombinedLoggingHandler(os.Stderr, Handlers() )
 
-    http.ListenAndServe(listenAddress, logged)
+  if err := http.ListenAndServe(listenAddress, logged); err != nil {
+    log.Fatal(err)
+  }
 }
