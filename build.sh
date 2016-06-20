d84e08ab3494f532569cc23b9237231013e4c197
Simas Cepaitis <simas.cepaitis@pearson.com>
2016-06-20 12:08:56 +0300

    Add go tests before docker build

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -e
 
 VERSION=0.0.1
+# run tests before build
+go test
 docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 bash -c "go get -d && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bitesize-authz-webhook -v"
 docker build -t geribatai/bitesize-authz-webhook:$VERSION .
 docker push geribatai/bitesize-authz-webhook:$VERSION
