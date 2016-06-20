a37a0768bd0cd5d43f45bdbe4ad07c51b99d6db3
Simas Cepaitis <simas.cepaitis@pearson.com>
2016-06-20 12:12:22 +0300

    Parametrize docker image

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-VERSION=0.0.1
+VERSION=${1:-0.0.1}
+DOCKER_IMAGE=geribatai/bitesize-authz-webhook:${VERSION}
+
 # run tests before build
 go test
+
 docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 bash -c "go get -d && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bitesize-authz-webhook -v"
-docker build -t geribatai/bitesize-authz-webhook:$VERSION .
-docker push geribatai/bitesize-authz-webhook:$VERSION
+docker build -t ${DOCKER_IMAGE} .
+docker push ${DOCKER_IMAGE}
