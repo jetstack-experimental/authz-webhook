a28168da4adf1f5f9fab9d1240e41c181625a501
Simas Cepaitis <simas.cepaitis@pearson.com>
2016-06-27 10:51:29 +0300

    Add git tagging

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ echo "Running go test..."
 go test
 
 echo "Building application..."
+git tag RELEASE-${VERSION}
+git push origin RELEASE-${VERSION}
 docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 bash -c "go get -d && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bitesize-authz-webhook -v"
 
 echo "Building docker image..."
